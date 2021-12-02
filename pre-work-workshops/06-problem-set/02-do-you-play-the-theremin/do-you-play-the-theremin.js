443790ff28c4e7f39616f992d6581308597c7970
Dan TeVelde <dan.tevelde@comcast.net>
2021-12-02 14:29:13 -0800

    commit for testing 06/02

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,3 +1,10 @@
 // YOUR CODE BELOW
-// function DoYouPlayTheTheremin(string)
-//    
+function DoYouPlayTheTheremin(string1) {
+    let result = '';
+    if (string1[0] === 'S' || string1[0] === 's') {
+        result = true;
+    } else {
+        result = false;
+    }
+    return
+}
