ec767911a1dbbc667850bd8274f6faf10545dcf4
Dan TeVelde <dan.tevelde@comcast.net>
2021-12-02 13:35:31 -0800

    commit for testing

--- pre-work-workshops/06-problem-set/01-greeting/greeting.js
+++ pre-work-workshops/06-problem-set/01-greeting/greeting.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function myGreeting(name) {
+let result = '';
+if (name === undefined) {
+    result = 'hello!';
+} else {
+    result = 'hello ', + name, + '!';
+}
+return result;
+}
--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,3 @@
 // YOUR CODE BELOW
+// function DoYouPlayTheTheremin(string)
+//    
